9cda7655f3bd29dc903e3936c99e22237d728fd3
Rafael Chocoj <rafaelcliveguitar@gmail.com>
2019-11-19 04:26:22 -0600

    nodo venta

---
+++ src/proyectoclase/NodeVenta.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoclase;
+
+import java.util.Date;
+
+/**
+ *
+ * @author RAFAEL
+ */
+public class NodeVenta {
+    
+    String tipo;
+    
+    Date horas_x;
+    int idcliente_y;
+    Date fecha_z;
+    String cliente;
+    String usuario;
+    
+    double total;
+    double monto;
+    double vuelto;
+
+    NodeVenta right;
+    NodeVenta left;
+    NodeVenta down;
+    NodeVenta up;
+    
+    NodeVenta capa_up;
+    NodeVenta capa_down;
+    
+    public NodeVenta (String tipo, Date horas_x, int idcliente_y, Date fecha_z, String cliente, String usuario, double total, double monto, double vuelto){
+        this.tipo = tipo;
+        this.horas_x = horas_x;
+        this.idcliente_y = idcliente_y;
+        this.fecha_z = fecha_z;
+        
+        this.cliente = cliente;
+        this.usuario = usuario;
+        
+        this.total = total;
+        this.monto = monto;
+        this.vuelto = vuelto;
+    }
+    
+}
